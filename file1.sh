6899eb37efe9c031e4fed45caf2fee8baccba178
mk3709 <mk3709@srmist.edu.in>
2024-01-16 12:37:00 +0530

    addition: subraction feature

--- file1.sh
+++ file1.sh
@@ -2,15 +2,18 @@ echo "enter num1"
 read a
 echo "enter num2"
 read b
-echo "enter 1 for addition"
+echo "enter 1 for addition\n 2 for sub"
 read input
 
 
 case $input in
 
-        1) sum-$(($a+$b))
-         echo $sum        
+        1) sum=$(($a+$b))
+        echo $sum        
         ;;
+        2) sub=$(($a-$b))
+        echo $sub 
+        ;;        
         *)echo "invalid"
 esac
 
